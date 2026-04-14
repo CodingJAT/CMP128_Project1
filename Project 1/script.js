1c54ad436797a0f071dd54be033c67bd89fb92f6
CodingJAT <jezrelt1220@gmail.com>
2026-04-14 01:00:45 -0400

    Add files via upload

---
+++ Project 1/script.js
@@ -0,0 +1,6 @@
+window.alert("Choose wisely... Jedi or Sith?");
+console.log("Welcome to this webseite!");
+
+function JvS() {
+  window.open("https://www.sideshow.com/blog/star-wars-jedi-sith-philosophy", "_blank");
+}
